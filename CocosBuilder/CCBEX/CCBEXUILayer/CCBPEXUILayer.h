3fe4d231d12caebade8cc3ddc476fe1d9f29d0fa
bmanGH <zx123xz321hm3@hotmail.com>
2014-03-18 18:24:07 +0800

    加入CCBEX::UILayer Plug-in;

---
+++ CocosBuilder/CCBEX/CCBEXUILayer/CCBPEXUILayer.h
@@ -0,0 +1,10 @@
+
+#import "cocos2d.h"
+
+@interface CCBPEXUILayer : CCLayer
+
+@property (nonatomic, readwrite, assign) BOOL enableTouch;
+@property (nonatomic, readwrite, assign) BOOL isSwallowTouches;
+@property (nonatomic, readwrite, assign) BOOL enableMultipleTouch;
+
+@end
---
+++ CocosBuilder/CCBEX/CCBEXUILayer/CCBPEXUILayer.m
@@ -0,0 +1,10 @@
+
+#import "CCBPEXUILayer.h"
+
+@implementation CCBPEXUILayer
+
+@synthesize enableTouch;
+@synthesize isSwallowTouches;
+@synthesize enableMultipleTouch;
+
+@end
